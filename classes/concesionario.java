f61f320a7f3f94c434268357928d6ac33ec74c18
miguel angel romero parra <mr9501459@gmail.com>
2024-09-25 12:29:16 -0500

    brand loop

--- classes/concesionario.java
+++ classes/concesionario.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class concesionario {
+     //-----------------------------------Vehicle Object------------------------------------------
      class Vehicle{
           HashMap<String, Object> VehicleData = new HashMap<>(); //Hashmap who will store the Data of vehicle
 
@@ -18,11 +19,13 @@ public class concesionario {
                VehicleData.put("Availability",available);
           }
 
+          //---get vehicle hashmap 
           public HashMap<String,Object> getVehicleData_HashMap(){
                return this.VehicleData;
           }
      }
 
+     //-----------------------------------------Dealership Object --------------------------------------------
      class DealerShip{
           HashMap<String, HashMap<String,Object>> Inventory = new HashMap<>(); //Hashmap to store the inventroy of dealership
 
@@ -30,7 +33,7 @@ public class concesionario {
                this.Inventory = new HashMap<>();
           }
 
-          //Show menu function
+          //---Show menu function
           public void showMenu(){
                System.out.println("\nEnter a option: ");
                System.out.println("1. See all products");
@@ -39,8 +42,7 @@ public class concesionario {
                System.out.println("4. Exit");
           }
 
-          //MAIN FUNCTION: Show all the user options and connect the other objects for getting information for inventory
-          public void dealerShip_Options(Scanner scannerUser){
+          public void dealerShip_Options(Scanner scannerUser, DealerShip dealerShip){
 
                while(true){
                     showMenu();
@@ -50,55 +52,96 @@ public class concesionario {
                     switch (Option_Chosen_ByUser) {
                          case 1-> getInventory();
                          
-                         case 2 -> filter(scannerUser);
+                         case 2 -> filter(scannerUser,dealerShip);
 
                          case 3 -> AddProduct(scannerUser);
                          
                          case 4 -> exit();
                          
                          default-> throw new AssertionError();
-               }
+                    }
                }
           }
 
 
-          //Get inventory 
+          //---Get inventory 
           public HashMap<String,HashMap<String,Object>> getInventory(){
                System.out.println(this.Inventory);
                return this.Inventory;
           }
           
 
-          //Show menu abot filter
+          //---Show menu about filter
           public void ShowMenu_Filter(){
-               System.err.println("Choose one option: ");
-               System.err.println("Filter by brand: ");
-               System.err.println("Filter by type: ");
-               System.err.println("Filter by price: ");
-               System.err.println("Filter by availability: ");
+               System.out.println("Choose one option: ");
+               System.out.println("Filter by brand: ");
+               System.out.println("Filter by type: ");
+               System.out.println("Filter by price: ");
+               System.out.println("Filter by availability: ");
           }
 
 
-          //filter
-          public void filter(Scanner scannerUser){
+          //---filter options
+          public void filter(Scanner scannerUser, DealerShip dealerShip){
                ShowMenu_Filter();
                int Filter_ByUser = scannerUser.nextInt();
 
                switch(Filter_ByUser){
-                    case 3 -> filterPrice(scannerUser);
+                    case 1 -> filterBrand(scannerUser, dealerShip);
+                    case 3 -> filterPrice(scannerUser, dealerShip);
                }
           }
 
-          //filter by price
-          public void filterPrice(Scanner scannerUser){
+          //---filter by price
+          public void filterPrice(Scanner scannerUser, DealerShip dealerShip){
+               scannerUser.nextLine();
                System.out.println("Valor mayor a: ");
                BigDecimal filter_price_bigDecimal = scannerUser.nextBigDecimal();
                scannerUser.nextLine();
 
+               //CONTINUAR BUCLE DE FILTRAR POR PRECIO DESPUES
                
           }
 
-          //Add Vehicle function
+          //---filter by brand
+          public void filterBrand(Scanner scanner, DealerShip dealerShip){
+               scanner.nextLine();
+               System.out.println("Enter your brand: ");
+               String filter_brand = scanner.nextLine();
+
+               //loop to filter the brand in all dictionary
+               for (var entry : dealerShip.getInventory().entrySet()) {
+                         String model = entry.getKey(); // get key
+
+                         if(filter_brand.equals(model)){
+                         HashMap<String, Object> vehicleData = entry.getValue(); // get value
+                    
+                         //print vehicle data
+                         System.out.println("Modelo: " + model);
+                         System.out.println("Datos del vehículo: " + vehicleData);
+                    }
+                }
+                
+          }
+
+          //---filter by type
+          public void filterType(Scanner scanner, DealerShip dealerShip){
+               System.out.println("Enter your vehicle type");
+               String filter_Type = scanner.nextLine();
+
+               //crear bucle para filtrar por tipo
+          }
+
+          //---filter by availability
+          public void filterAvailability(Scanner scanner, DealerShip dealerShip){
+               System.out.println("Enter the Vehicle Availability");
+               String filter_Availability = scanner.nextLine();
+
+               //crear bucle para filtar por disponibilidad
+          }
+
+
+          //---Add Vehicle function
           public void AddProduct(Scanner scannerUser){
 
                //model
@@ -125,19 +168,19 @@ public class concesionario {
                boolean Vehicle_Availability;
 
               //condition to know if the Vehicle is available (true/false)
-              Vehicle_Availability = vehicleAvailabilityInput.equals("y");
+               Vehicle_Availability = vehicleAvailabilityInput.equals("y");
                
                Vehicle newVehicle = new Vehicle(Vehicle_Brand, Vehicle_Type, Vehicle_Price, Vehicle_Availability); //object creation
                
                setInventorty_HashMap(newVehicle.getVehicleData_HashMap(),Vehicle_Model); //(Hashmap <string,object> from the class "Vehicle",Vehicle model by user)
           }
 
-          //Set the new vehicle in the actualInventory
+          //---Set the new vehicle in the actualInventory
           public void setInventorty_HashMap(HashMap<String, Object> Send_To_Inventory,String model){
                this.Inventory.put(model, Send_To_Inventory);
           }
 
-          //exit function
+          //---exit function
           public void exit(){
                System.out.println("Thanks. Come back soon");
                System.exit(0);
@@ -150,7 +193,7 @@ public class concesionario {
           //try execute program
           try (Scanner scannerUser = new Scanner(System.in)){
                DealerShip dealerShip = concesionario.new DealerShip(); //Dealeship object creation
-               dealerShip.dealerShip_Options(scannerUser); 
+               dealerShip.dealerShip_Options(scannerUser,dealerShip); 
           } 
           catch (Exception e) { System.out.println("Erroe encontrado: "+e);}
      }
